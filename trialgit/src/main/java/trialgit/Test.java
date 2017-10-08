c32c9564511fe2cbe65a6a974fcbbe6e9bdcbc89
Sushant <Sushant@Sushant-LAP>
2017-10-08 16:14:51 +0530

    Applying stash

--- trialgit/src/main/java/trialgit/Test.java
+++ trialgit/src/main/java/trialgit/Test.java
@@ -4,6 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, My first git repo project...!!!!");
+		System.out.println("For stashes...!!!!");
 		
 	}
 
