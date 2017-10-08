1cf6101635f892f2fe9882544c383e12f4650341
Sushant <Sushant@Sushant-LAP>
2017-10-08 16:10:40 +0530

    small change

--- trialgit/src/main/java/trialgit/Test.java
+++ trialgit/src/main/java/trialgit/Test.java
@@ -3,7 +3,7 @@ package trialgit;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, My first git repo project...!!!");
+		System.out.println("Hello, My first git repo project...!!!!");
 		
 	}
 
